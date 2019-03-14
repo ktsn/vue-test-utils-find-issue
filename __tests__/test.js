d099266e6c5d46b3764e6c118eef55ffabe36d3f
ktsn <ktsn55@gmail.com>
2019-03-14 14:59:17 +0800

    use mount method instead

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,14 +1,10 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import Test from '../src/Test'
 import Wrapper from '../src/Wrapper'
 import Child from '../src/Child'
 
 it('should find child component', () => {
-  const wrapper = shallowMount(Test, {
-    stubs: {
-      Wrapper
-    }
-  })
+  const wrapper = mount(Test)
 
   const found = wrapper.find('[data-test-id=child]')
   found.vm.$emit('click')
@@ -17,11 +13,7 @@ it('should find child component', () => {
 })
 
 it('workaround', () => {
-  const wrapper = shallowMount(Test, {
-    stubs: {
-      Wrapper
-    }
-  })
+  const wrapper = mount(Test)
 
   const found = wrapper
     .findAll(Child)
